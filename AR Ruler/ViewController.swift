0f4f6e5439f1eeb19a5c1d6a67d7a50023d72a32
Kaffka <kaffka.anastasia@gmail.com>
2023-05-15 16:21:29 +0200

    Finish ruler app

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -12,7 +12,8 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
-    
+    var dotNodes = [SCNNode]()
+    var textNode = SCNNode()
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,24 +21,87 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+       // sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
-        sceneView.scene = scene
-        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+       
+      //  sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("Touched")
+        if dotNodes.count >= 2 {
+            for dot in dotNodes{
+                dot.removeFromParentNode()
+            }
+            dotNodes = [SCNNode]()
+            textNode.removeFromParentNode()
+        }
+        
+        if let touchLocation = touches.first?.location(in: sceneView){
+            guard let query = sceneView.raycastQuery(from: touchLocation, allowing: .existingPlaneInfinite, alignment: .any) else {return}
+            let results = sceneView.session.raycast(query)
+            guard let hitTestResult = results.first
+            else {
+                print("No surface found")
+                return
+            }
+            sceneView.scene.rootNode.addChildNode(addDot(at: hitTestResult))
+            
+          
+        }
+    }
+    func addDot(at location: ARRaycastResult ) -> SCNNode {
+        let sphere = SCNSphere(radius: 0.002)
+        let sphereNode = SCNNode()
+        sphereNode.geometry = sphere
+        sphereNode.position = SCNVector3(x: Float(location.worldTransform.columns.3.x),
+                                         y: Float(location.worldTransform.columns.3.y + sphereNode.boundingSphere.radius),
+                                         z: Float(location.worldTransform.columns.3.z))
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.magenta
+        sphereNode.geometry?.materials = [material]
+        dotNodes.append(sphereNode)
+        if dotNodes.count >= 2 {
+            calculate()
+        }
+        
+        return sphereNode
+    }
+    func calculate(){
+        let start = dotNodes[0]
+        let end = dotNodes[1]
+        
+        print(start.position)
+        print(end.position)
+        
+        let a = end.position.x - start.position.x
+        let b = end.position.y - start.position.y
+        let c = end.position.z - start.position.z
+        
+        let distance = sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
+        print(abs(distance))
+       
+updateText(text: "\(String(format: "%.1f", distance * 100)) cm", atPosition: start.position)
+    }
+   
+    func updateText(text: String, atPosition position : SCNVector3){
+       
+        
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.magenta
+
+        textGeometry.font =   UIFont(name: "Helvetica", size: 80)
+        textNode.geometry = textGeometry
+        textNode.position = SCNVector3(x: position.x, y: position.y + 0.03 , z: position.z)
+        textNode.scale = SCNVector3(0.0004, 0.0004, 0.0004)
+        sceneView.scene.rootNode.addChildNode(textNode)
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        configuration.planeDetection = .horizontal
         // Run the view's session
         sceneView.session.run(configuration)
     }
